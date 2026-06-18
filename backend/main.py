df6a90c2dc3a565cf8e08f970f89d0b53b4fa0fc
Quant Partners <thequantpartners@gmail.com>
2026-06-18 14:48:08 -0500

    fix: allow any vercel subdomains in CORS

--- backend/main.py
+++ backend/main.py
@@ -88,6 +88,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=FRONTEND_ORIGINS,
+    allow_origin_regex=r"https://googlemaker-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
